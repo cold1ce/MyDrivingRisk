38c02ee7b4c71ae9949dc880b48df867009b6ce8
cold1ce <Julian.Ruppert@gmail.com>
2016-11-15 20:57:10 +0100

    GPS Funktionen eingebaut

--- app/src/main/java/fim/de/mydrivingrisk/GPSDatenTest.java
+++ app/src/main/java/fim/de/mydrivingrisk/GPSDatenTest.java
@@ -1,14 +1,119 @@
 package fim.de.mydrivingrisk;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class GPSDatenTest extends AppCompatActivity {
+
+
+    private Button b1;
+    private TextView t1;
+    private TextView t2;
+    private TextView t3;
+    private TextView t4;
+    private TextView t5;
+    private TextView t6;
+    private LocationManager locationManager1;
+    private LocationListener listener1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpsdaten_test);
         setTitle("My Driving Risk - GPS Daten Test");
+
+        t1 = (TextView) findViewById(R.id.textView10);
+        t2 = (TextView) findViewById(R.id.textView11);
+        t3 = (TextView) findViewById(R.id.textView12);
+        t4 = (TextView) findViewById(R.id.textView13);
+        t5 = (TextView) findViewById(R.id.textView14);
+        t6 = (TextView) findViewById(R.id.textView15);
+        b1 = (Button) findViewById(R.id.button5);
+
+        locationManager1 = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        listener1 = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Calendar kalender = Calendar.getInstance();
+                SimpleDateFormat zeitformat = new SimpleDateFormat("HH:mm:ss");
+                t6.setText(zeitformat.format(kalender.getTime()));
+
+                t2.setText(" " + location.getLatitude());
+                t4.setText(" " + location.getLongitude());
+                //t.append("\n " + location.getLongitude() + " " + location.getLatitude());
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        configure_button();
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                locationManager1.requestLocationUpdates("gps", 2000, 0, listener1);
+            }
+        });
+    }
+
+
+
 }
