28cec0907202b46e2870b35d57a14f27dbe0216e
cold1ce <Julian.Ruppert@gmail.com>
2016-11-15 16:19:55 +0100

    Design

---
+++ app/src/main/java/fim/de/mydrivingrisk/GPSDatenTest.java
@@ -0,0 +1,14 @@
+package fim.de.mydrivingrisk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GPSDatenTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpsdaten_test);
+        setTitle("My Driving Risk - GPS Daten Test");
+    }
+}
