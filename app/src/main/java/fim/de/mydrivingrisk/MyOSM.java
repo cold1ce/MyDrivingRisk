97145b32dd023cfe8118d2c6dd74e2eb879cf249
Beneneu <bene.neubert@googlemail.com>
2016-12-29 17:08:47 +0100

    OSM Algorithmus überarbeitet

--- app/src/main/java/fim/de/mydrivingrisk/MyOSM.java
+++ app/src/main/java/fim/de/mydrivingrisk/MyOSM.java
@@ -2,25 +2,16 @@
 package fim.de.mydrivingrisk;
 
 import android.os.AsyncTask;
-
-
-
-import org.json.*;
-
-
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-
-import java.net.URL;
-
-
-import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 
@@ -66,104 +57,95 @@ public class MyOSM {
                 if (jsoninput != null) {
                     System.out.println("JSON Paket ist nicht leer :-)");
                     JSONObject object = jsoninput;
-                    System.out.println("JSON Object 1: "+object);
+                    System.out.println("JSON Object 1: " + object);
 
                     JSONArray array1 = jsoninput.getJSONArray("elements");
-                    System.out.println("Elements Array: "+array1);
+                    System.out.println("Elements Array: " + array1);
 
-                    //for(int i = 0; i < array1.length(); i++)
-                    //{
-                        JSONObject object3 = array1.getJSONObject(0);
-                        System.out.println("Object3 : "+object3);
-
-                        //JSONArray array2 = object3.getJSONArray("tags");
-                        //System.out.println("Array2 : "+array2);
-
-                        String zwo = object3.getString("tags");
-                        System.out.println("String zwo/tags : "+zwo);
-
-                        JSONObject object4 = new JSONObject(zwo);
-                        System.out.println("Object 4 : "+object4);
-
-
-
-
-
+                    for (int i = 0; i < array1.length(); i++) {
                         try {
-                            maxspeed = object4.getDouble("maxspeed");
-                            System.out.println("MAXSPEED: " + maxspeed);
-                        }
-                        catch (JSONException e){
-                            System.out.println("kein maxspeed-wert erhalten");
-                            maxspeed = 0.0;
-                        }
+                            JSONObject object3 = array1.getJSONObject(i);
+                            System.out.println("Object3 : " + object3);
 
-                        try {
-                            strasse = object4.getString("name");
-                            System.out.println("Straße: "+strasse);
-                        }
-                        catch (JSONException e){
-                            System.out.println("kein strasse-wert erhalten");
+                            //JSONArray array2 = object3.getJSONArray("tags");
+                            //System.out.println("Array2 : "+array2);
+
+                            String zwo = object3.getString("tags");
+                            System.out.println("String zwo/tags : " + zwo);
+
+                            JSONObject object4 = new JSONObject(zwo);
+                            System.out.println("Object 4 : " + object4);
+
+
                             try {
-                                strasse = object4.getString("ref");
-                                System.out.println("Straße: "+strasse);
+                                maxspeed = object4.getDouble("maxspeed");
+                                System.out.println("MAXSPEED: " + maxspeed);
+                            } catch (JSONException e) {
+                                System.out.println("kein maxspeed-wert erhalten");
+                                maxspeed = 0.0;
                             }
-                            catch (JSONException f){
-                                System.out.println("kein ref-wert erhalten");
+
+                            try {
+                                strasse = object4.getString("name");
+                                System.out.println("Straße: " + strasse);
+                            } catch (JSONException e) {
+                                System.out.println("kein strasse-wert erhalten");
+                                try {
+                                    strasse = object4.getString("ref");
+                                    System.out.println("Straße: " + strasse);
+                                } catch (JSONException f) {
+                                    System.out.println("kein ref-wert erhalten");
+                                }
+                            }
+
+                            try {
+                                strassentyp = object4.getString("highway");
+                                System.out.println("Straßentyp: " + strassentyp);
+                            } catch (JSONException e) {
+                                System.out.println("kein straßentyp-wert erhalten");
                             }
                         }
-
-                        try {
-                            strassentyp = object4.getString("highway");
-                            System.out.println("Straßentyp: "+strassentyp);
-                        }
-                        catch (JSONException e){
-                            System.out.println("kein straßentyp-wert erhalten");
+                        catch (JSONException e) {
                         }
 
 
+                        //}
 
 
+                        //JSONArray object2 = new JSONArray(one);
+                        //System.out.println("JSON Object 2: "+object2);
+
+                        //String two = object2.getString(0);
+                        //System.out.println("Two String: "+two);
+
+                        //JSONArray object3 = new JSONArray(two);
+                        //System.out.println("JSON Object 3: "+object3);
 
 
-                    //}
+                        //System.out.println("Tags String: "+one);
+
+                        //JSONArray jArray1 = object.getJSONArray("tags");
+                        //String abc = object2.getString("maxspeed");
+
+                        //System.out.println("Det: "+abc);
+                        //System.out.println("JSON ist nicht Null. top.");
+                        //JSONObject details = jsoninput.getJSONArray("elements").getJSONObject(0);
+                        //JSONObject details2 = details.getJSONArray("tags");
+
+                        //System.out.println("Details: "+details);
+                        //System.out.println("Details: "+details2);
 
 
-                    //JSONArray object2 = new JSONArray(one);
-                    //System.out.println("JSON Object 2: "+object2);
-
-                    //String two = object2.getString(0);
-                    //System.out.println("Two String: "+two);
-
-                    //JSONArray object3 = new JSONArray(two);
-                    //System.out.println("JSON Object 3: "+object3);
-
+                        //String description = details.getString("id");
+                        //long sunrise = json.getJSONObject("sys").getLong("sunrise") * 1000;
+                        //long sunset = json.getJSONObject("sys").getLong("sunset") * 1000;
+                        //String description = details.getString("id");
+                        //String description2 = "desc2";
+                        //String description2 = details2.getString("maxspeed");
+                        System.out.println("Ausgabe: " + maxspeed + " | " + strasse + " | " + strassentyp);
 
 
-                    //System.out.println("Tags String: "+one);
-
-                    //JSONArray jArray1 = object.getJSONArray("tags");
-                    //String abc = object2.getString("maxspeed");
-
-                    //System.out.println("Det: "+abc);
-                    //System.out.println("JSON ist nicht Null. top.");
-                    //JSONObject details = jsoninput.getJSONArray("elements").getJSONObject(0);
-                    //JSONObject details2 = details.getJSONArray("tags");
-
-                    //System.out.println("Details: "+details);
-                    //System.out.println("Details: "+details2);
-
-
-
-                    //String description = details.getString("id");
-                    //long sunrise = json.getJSONObject("sys").getLong("sunrise") * 1000;
-                    //long sunset = json.getJSONObject("sys").getLong("sunset") * 1000;
-                    //String description = details.getString("id");
-                    //String description2 = "desc2";
-                    //String description2 = details2.getString("maxspeed");
-                    System.out.println("Ausgabe: "+maxspeed+" | "+strasse+" | "+strassentyp);
-
-
+                    }
                 }
                 else {
                     System.out.println("JSON Paket ist leer :-(");
@@ -171,17 +153,19 @@ public class MyOSM {
                     strasse = "not found";
                     strassentyp = "not found";
                 }
-            } catch (JSONException e) {
-                    maxspeed = 0.0;
-                    strasse = "not found";
-                    strassentyp = "not found";
-                    Log.e("FEHLER", "Cannot process JSON results", e);
-                    System.out.println("OSM-Abfrage-Exception");
+
+            delegate.processFinish(maxspeed, strasse, strassentyp);
+        } catch (JSONException e) {
+                maxspeed = 0.0;
+                strasse = "not found";
+                strassentyp = "not found";
+                Log.e("FEHLER", "Cannot process JSON results", e);
+                System.out.println("OSM-Abfrage-Exception");
+                e.printStackTrace();
+            }
 
             }
-            delegate.processFinish(maxspeed, strasse, strassentyp);
         }
-    }
 
 
     public static JSONObject getTempoJSON(String lat, String lon) {
